b102ed85d702542ee1939de80e79f67338d6b2b7
Gabor Szarnyas <szarnyasg@gmail.com>
2015-12-03 16:35:29 +0100

    WIP

--- plot.R
+++ plot.R
@@ -1,34 +1,7 @@
 library("ggplot2")
 
 benchmark.plot = function(df, scenario, modelsizes, title, facet, scale, ncol, width = 210, height = 297) {
-  # x axis labels
-  modelsizes.scenario = modelsizes[modelsizes$Scenario == scenario, "Triples"]
 
-  xbreaks = modelsizes[modelsizes$Scenario == scenario, "Artifact"]
-  xlabels = paste(xbreaks, "\n", modelsizes.scenario, sep = "")
-  
-  # y axis labels
-  ys = -10:10
-  ybreaks = 10^ys
-  ylabels = parse(text = paste("10^", ys, sep = ""))
-
-  plot.filename = gsub(" ", "-", title)
-
-  facet = as.formula(paste("~", facet))
-
-  p = ggplot(df) +
-    labs(title = paste(scenario, " scenario, ", title, sep = ""), x = "model size\n#triples", y = "execution time [s]") +
-    geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
-    geom_line(aes(x = as.factor(Artifact), y = time, col = Tool, group = Tool), size = 0.15) +
-    scale_shape_manual(values = seq(0,24)) +
-    scale_x_discrete(breaks = xbreaks, labels = xlabels) +
-    scale_y_log10(breaks = ybreaks, labels = ylabels) +
-    facet_wrap(facet, ncol = ncol, scale = scale) +
-    theme_bw() +
-    theme(legend.key = element_blank(), legend.title = element_blank(), legend.position = "bottom") +
-    guides(shape = guide_legend(ncol = 2))
-  
-  ggsave(file = paste("diagrams/", scenario, "-", plot.filename, ".pdf", sep = ""), width = width, height = height, units = "mm")
 }
 
 benchmark.plot.by.phase = function(df, scenario, modelsizes, levels, case, title, ncol = 2) {
--- report.R
+++ report.R
@@ -9,7 +9,7 @@
 # * filter the data
 # * convert the data from long table to wide table (better suited for processing)
 # * aggregate the data
-# * convert the data to long table which (better suited for visualization)
+# * convert the data to long table (better suited for visualization)
 # * draw the plots
 
 library("reshape2")
@@ -19,8 +19,9 @@ source("plot.R")
 
 results = read.csv("results.csv")
 
+
 # filtering for time values
-times = subset(results, Metric == "Time")
+times = subset(results, Metric == "time")
 
 # convert nanoseconds to seconds
 times$Value = times$Value / 10^9
@@ -28,39 +29,7 @@ times$Value = times$Value / 10^9
 times$Tool = gsub('_', ' ', times$Tool)
 
 # transform long table to wide table
-times.wide = dcast(times,
-                   Scenario + Tool + Run + Case + Artifact + Metric ~ Phase,
-                   value.var = "Value")
-
-# calculate aggregated values
-times.derived = times.wide
-times.derived$Read.and.Check = times.derived$Read + times.derived$Check
-
-# summarize for each value (along the **Iteration** attribute) using a columnwise function
-times.aggregated.iteration = ddply(
-  .data = times.derived,
-  .variables = c("Scenario", "Tool", "Run", "Case", "Artifact", "Metric"),
-  .fun = colwise(mean)
-)
-
-# summarize for each value (along the **Run** attribute) using the fr function
-times.aggregated.runs = ddply(
-  .data = times.aggregated.iteration,
-  .variables = c("Scenario", "Tool", "Case", "Artifact", "Metric"),
-  .fun = colwise(median)
-)
-
-# melt data to a wide table
-times.plot = melt(
-  data = times.aggregated.runs,
-  id.vars = c("Scenario", "Tool", "Case", "Artifact", "Metric"),
-  measure.vars = c("Read", "Check", "Read.and.Check"),
-  variable.name = "Phase",
-  value.name = "time"
-)
-
-# remove the . characters from the phasename
-times.plot$Phase = gsub('\\.', ' ', times.plot$Phase)
+times.plot = times
 
 # modelsizes
 modelsize.batch  = data.frame(Scenario = "Batch",  Artifact = 2^(0:14), Triples = c("4.7k", "7.9k", "20.6k", "41k", "89.4k", "191.8k", "374.1k", "716.5k", "1.5M", "2.8M", "5.7M", "11.5M", "23M", "45.9M", "92.3M"))
@@ -69,10 +38,57 @@ modelsize.repair = data.frame(Scenario = "Repair", Artifact = 2^(0:14), Triples 
 modelsizes = do.call(rbind, list(modelsize.batch, modelsize.inject, modelsize.repair))
 
 # levels for the facets in the plot
-levels.cases = c("RouteSensor", "SwitchSet", "ConnectedSegments", "SemaphoreNeighbor")
+levels.cases = c("PosLength", "SwitchSensor", "RouteSensor", "SwitchSet", "ConnectedSegments", "SemaphoreNeighbor")
 
 # draw plots
-benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "Read", "read phase")
-benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "Check", "check phase")
-benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "Read and Check", "read and check phase")
+#benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "read", "read phase")
+
+scenario = "Batch"
+
+# x axis labels
+modelsizes = modelsize.repair
+
+xbreaks = modelsizes$Artifact
+xbreaks
+xlabels = paste(xbreaks, "\n", modelsizes$Triples, sep = "")
+xlabels
+
+# y axis labels
+ys = -10:10
+ybreaks = 10^ys
+ylabels = parse(text = paste("10^", ys, sep = ""))
+
+facet = "Case"
+facet = as.formula(paste("~", facet))
+title = "MyPlot"
+
+width = 210
+height = 297
+
+df = times.plot
+df = df[df$Phase == "read", ]
+df$Case = factor(df$Case, levels = levels.cases)
+
+df
+drops = c("Run","Iteration")
+df = df[,!(names(df) %in% drops)]
+df
+ggplot(df) + 
+  #geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
+  geom_line(aes(x = as.factor(Artifact), y = Value, col = Tool, group = Tool), size = 0.15)
+  
+p = ggplot(df) +
+  labs(title = paste(scenario, " scenario, ", title, sep = ""), x = "model size\n#triples", y = "execution time [s]") +
+  geom_point(aes(x = as.factor(Artifact), y = Value, col = Tool, shape = Tool), size = 1.5) +
+  geom_line(aes(x = as.factor(Artifact), y = Value, col = Tool, group = Tool), size = 0.15) +
+  scale_shape_manual(values = seq(0,24)) +
+  facet_wrap(facet, ncol = 2, scale = "fixed") +
+  theme_bw() +
+  theme(legend.key = element_blank(), legend.title = element_blank(), legend.position = "bottom")
+print(p)
+p
+
+plot.filename = gsub(" ", "-", title)
+plot.filename = paste("diagrams/", scenario, "-", plot.filename, ".pdf", sep = "")
+ggsave(file = plot.filename, width = width, height = height, units = "mm")
 
