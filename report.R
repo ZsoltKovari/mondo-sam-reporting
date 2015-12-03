93c53aee8c7749137e1883190bd328e81d923863
Gabor Szarnyas <szarnyasg@gmail.com>
2015-12-03 17:03:45 +0100

    Script running

--- report.R
+++ report.R
@@ -30,36 +30,23 @@ times$Tool = gsub('_', ' ', times$Tool)
 
 # transform long table to wide table
 times.plot = times
+head(times.plot)
+times.plot = rename(times.plot, c("Value"="time"))
+head(times.plot)
 
-# modelsizes
-modelsize.batch  = data.frame(Scenario = "Batch",  Artifact = 2^(0:14), Triples = c("4.7k", "7.9k", "20.6k", "41k", "89.4k", "191.8k", "374.1k", "716.5k", "1.5M", "2.8M", "5.7M", "11.5M", "23M", "45.9M", "92.3M"))
-modelsize.inject = data.frame(Scenario = "Inject", Artifact = 2^(0:14), Triples = c("5k", "9.3k", "19.9k", "44.6k", "85.7k", "191.6k", "373.1k", "752.8k", "1.5M", "3M", "5.8M", "11.6M", "23.3M", "46.5M", "93M"))
-modelsize.repair = data.frame(Scenario = "Repair", Artifact = 2^(0:14), Triples = c("4.9k", "9.3k", "19.8k", "44.5k", "85.4k", "191.1k", "372.1k", "750.7k", "1.5M", "2.9M", "5.8M", "11.5M", "23.2M", "46.4M", "92.8M"))
-modelsizes = do.call(rbind, list(modelsize.batch, modelsize.inject, modelsize.repair))
-
-# levels for the facets in the plot
-levels.cases = c("PosLength", "SwitchSensor", "RouteSensor", "SwitchSet", "ConnectedSegments", "SemaphoreNeighbor")
-
-# draw plots
-#benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "read", "read phase")
-
+# plot decoration
 scenario = "Batch"
-
+modelsizes = data.frame(Scenario = "Repair", Artifact = 2^(0:14), Triples = c("4.9k", "9.3k", "19.8k", "44.5k", "85.4k", "191.1k", "372.1k", "750.7k", "1.5M", "2.9M", "5.8M", "11.5M", "23.2M", "46.4M", "92.8M"))
+levels.cases = c("PosLength", "SwitchSensor", "RouteSensor", "SwitchSet", "ConnectedSegments", "SemaphoreNeighbor")
 # x axis labels
-modelsizes = modelsize.repair
-
 xbreaks = modelsizes$Artifact
-xbreaks
 xlabels = paste(xbreaks, "\n", modelsizes$Triples, sep = "")
-xlabels
-
 # y axis labels
 ys = -10:10
 ybreaks = 10^ys
 ylabels = parse(text = paste("10^", ys, sep = ""))
 
-facet = "Case"
-facet = as.formula(paste("~", facet))
+facet = as.formula(paste("~", "Case"))
 title = "MyPlot"
 
 width = 210
@@ -69,24 +56,20 @@ df = times.plot
 df = df[df$Phase == "read", ]
 df$Case = factor(df$Case, levels = levels.cases)
 
-df
+head(df)
 drops = c("Run","Iteration")
 df = df[,!(names(df) %in% drops)]
-df
-ggplot(df) + 
-  #geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
-  geom_line(aes(x = as.factor(Artifact), y = Value, col = Tool, group = Tool), size = 0.15)
-  
 p = ggplot(df) +
   labs(title = paste(scenario, " scenario, ", title, sep = ""), x = "model size\n#triples", y = "execution time [s]") +
-  geom_point(aes(x = as.factor(Artifact), y = Value, col = Tool, shape = Tool), size = 1.5) +
-  geom_line(aes(x = as.factor(Artifact), y = Value, col = Tool, group = Tool), size = 0.15) +
+  geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
+  geom_line(aes(x = as.factor(Artifact), y = time, col = Tool, group = Tool), size = 0.15) +
   scale_shape_manual(values = seq(0,24)) +
+  scale_x_discrete(breaks = xbreaks, labels = xlabels) +
+  scale_y_log10(breaks = ybreaks, labels = ylabels) +
   facet_wrap(facet, ncol = 2, scale = "fixed") +
   theme_bw() +
   theme(legend.key = element_blank(), legend.title = element_blank(), legend.position = "bottom")
 print(p)
-p
 
 plot.filename = gsub(" ", "-", title)
 plot.filename = paste("diagrams/", scenario, "-", plot.filename, ".pdf", sep = "")
