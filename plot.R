893b59ef5c14068aec23c519b6271a23d3ab5977
Gabor Szarnyas <szarnyasg@gmail.com>
2015-12-03 17:58:52 +0100

    Make X axis sparser for two-column plots

--- plot.R
+++ plot.R
@@ -1,17 +1,51 @@
 library("ggplot2")
 
-benchmark.plot = function(df, scenario, modelsizes, title, facet, scale, ncol, width = 210, height = 297) {
-
+benchmark.plot = function(df, scenario, artifacts, title, facet, scale, ncol, width = 210, height = 297) {
+  if (ncol > 1) {
+    evens = seq(2, nrow(artifacts), by=2)
+    artifacts = artifacts[-evens, ]
+  }
+  
+  # x axis labels
+  artifacts.scenario = artifacts[artifacts$Scenario == scenario, "Triples"]
+  artifacts
+  
+  xbreaks = artifacts[artifacts$Scenario == scenario, "Artifact"]
+  xlabels = paste(xbreaks, "\n", artifacts.scenario, sep = "")
+  
+  # y axis labels
+  ys = -10:10
+  ybreaks = 10^ys
+  ylabels = parse(text = paste("10^", ys, sep = ""))
+  
+  plot.filename = gsub(" ", "-", title)
+  
+  facet = as.formula(paste("~", facet))
+  
+  p = ggplot(df) +
+    labs(title = paste(scenario, " scenario, ", title, sep = ""), x = "model size\n#triples", y = "execution time [s]") +
+    geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
+    geom_line(aes(x = as.factor(Artifact), y = time, col = Tool, group = Tool), size = 0.15) +
+    scale_shape_manual(values = seq(0,24)) +
+    scale_x_discrete(breaks = xbreaks, labels = xlabels) +
+    scale_y_log10(breaks = ybreaks, labels = ylabels) +
+    facet_wrap(facet, ncol = ncol, scale = scale) +
+    theme_bw() +
+    theme(legend.key = element_blank(), legend.title = element_blank(), legend.position = "bottom") + #, axis.text.x = element_text(size=8)) +
+    guides(shape = guide_legend(ncol = 2))
+  print(p)
+  
+  ggsave(file = paste("diagrams/", scenario, "-", plot.filename, ".pdf", sep = ""), width = width, height = height, units = "mm")
 }
 
-benchmark.plot.by.phase = function(df, scenario, modelsizes, levels, case, title, ncol = 2) {
+benchmark.plot.by.phase = function(df, scenario, artifacts, levels, case, title, ncol = 2) {
   df = df[df$Scenario == scenario & df$Case == case, ]
   df$Phase = factor(df$Phase, levels = levels)
-  benchmark.plot(df, scenario, modelsizes, title, "Phase", "free_y", ncol)
+  benchmark.plot(df, scenario, artifacts, title, "Phase", "free_y", ncol)
 }
 
-benchmark.plot.by.case = function(df, scenario, modelsizes, levels, phase, title, ncol = 2) {
+benchmark.plot.by.case = function(df, scenario, artifacts, levels, phase, title, ncol = 2) {
   df = df[df$Scenario == scenario & df$Phase == phase, ]
   df$Case = factor(df$Case, levels = levels)
-  benchmark.plot(df, scenario, modelsizes, title, "Case", "fixed", ncol)
+  benchmark.plot(df, scenario, artifacts, title, "Case", "fixed", ncol)
 }
--- report.R
+++ report.R
@@ -19,7 +19,6 @@ source("plot.R")
 
 results = read.csv("results.csv")
 
-
 # filtering for time values
 times = subset(results, Metric == "time")
 
@@ -30,48 +29,14 @@ times$Tool = gsub('_', ' ', times$Tool)
 
 # transform long table to wide table
 times.plot = times
-head(times.plot)
 times.plot = rename(times.plot, c("Value"="time"))
-head(times.plot)
 
 # plot decoration
 scenario = "Batch"
-modelsizes = data.frame(Scenario = "Repair", Artifact = 2^(0:14), Triples = c("4.9k", "9.3k", "19.8k", "44.5k", "85.4k", "191.1k", "372.1k", "750.7k", "1.5M", "2.9M", "5.8M", "11.5M", "23.2M", "46.4M", "92.8M"))
+# the scenario is set to "Batch" on purpose
+modelsizes.repair = data.frame(Scenario = "Batch", Artifact = 2^(0:14), Triples = c("4.9k", "9.3k", "19.8k", "44.5k", "85.4k", "191.1k", "372.1k", "750.7k", "1.5M", "2.9M", "5.8M", "11.5M", "23.2M", "46.4M", "92.8M"))
+modelsizes = do.call(rbind, list(modelsizes.repair))
+
 levels.cases = c("PosLength", "SwitchSensor", "RouteSensor", "SwitchSet", "ConnectedSegments", "SemaphoreNeighbor")
-# x axis labels
-xbreaks = modelsizes$Artifact
-xlabels = paste(xbreaks, "\n", modelsizes$Triples, sep = "")
-# y axis labels
-ys = -10:10
-ybreaks = 10^ys
-ylabels = parse(text = paste("10^", ys, sep = ""))
 
-facet = as.formula(paste("~", "Case"))
-title = "MyPlot"
-
-width = 210
-height = 297
-
-df = times.plot
-df = df[df$Phase == "read", ]
-df$Case = factor(df$Case, levels = levels.cases)
-
-head(df)
-drops = c("Run","Iteration")
-df = df[,!(names(df) %in% drops)]
-p = ggplot(df) +
-  labs(title = paste(scenario, " scenario, ", title, sep = ""), x = "model size\n#triples", y = "execution time [s]") +
-  geom_point(aes(x = as.factor(Artifact), y = time, col = Tool, shape = Tool), size = 1.5) +
-  geom_line(aes(x = as.factor(Artifact), y = time, col = Tool, group = Tool), size = 0.15) +
-  scale_shape_manual(values = seq(0,24)) +
-  scale_x_discrete(breaks = xbreaks, labels = xlabels) +
-  scale_y_log10(breaks = ybreaks, labels = ylabels) +
-  facet_wrap(facet, ncol = 2, scale = "fixed") +
-  theme_bw() +
-  theme(legend.key = element_blank(), legend.title = element_blank(), legend.position = "bottom")
-print(p)
-
-plot.filename = gsub(" ", "-", title)
-plot.filename = paste("diagrams/", scenario, "-", plot.filename, ".pdf", sep = "")
-ggsave(file = plot.filename, width = width, height = height, units = "mm")
-
+benchmark.plot.by.case(times.plot, "Batch", modelsizes, levels.cases, "read", "read phase")
